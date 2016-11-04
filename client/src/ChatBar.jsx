68874a3069d137cabc1b863f996fabf64ef5feef
Patrick Simonian <patricksimonian@gmail.com>
2016-11-04 14:11:44 -0700

    seperated colour grid into its own component

--- client/src/ChatBar.jsx
+++ client/src/ChatBar.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Colour_grid from './Colour-grid.jsx'
 
 class ChatBar extends Component {
   constructor(props) {
@@ -88,24 +89,7 @@ class ChatBar extends Component {
 
 
         <span className="textcolour-menu btn-group" onClick={this.handleMenuClick}>Colour</span>
-          <div className="colour-grid" ref={document.querySelector(".colour-grid")}>
-            <div className="colour-row">
-              <div className="colour-block" id="colour1" onClick={this.handleMenuClick} data-colour="#5853b2"></div>
-              <div className="colour-block" id="colour2" onClick={this.handleMenuClick} data-colour="#ad67d3"></div>
-              <div className="colour-block" id="colour3" onClick={this.handleMenuClick} data-colour="#d367bc"></div>
-            </div>
-            <div className="colour-row">
-              <div className="colour-block" id="colour4" onClick={this.handleMenuClick} data-colour="#bc2323"></div>
-              <div className="colour-block" id="colour5" onClick={this.handleMenuClick} data-colour="#23b7bc"></div>
-              <div className="colour-block" id="colour6" onClick={this.handleMenuClick} data-colour="#23bc5b"></div>
-            </div>
-             <div className="colour-row">
-              <div className="colour-block" id="colour7" onClick={this.handleMenuClick} data-colour="#727554"></div>
-              <div className="colour-block" id="colour8" onClick={this.handleMenuClick} data-colour="#af6c28"></div>
-              <div className="colour-block" id="colour9" onClick={this.handleMenuClick} data-colour=" black"></div>
-            </div>
-
-        </div>
+        <Colour_grid handleMenuClick={this.handleMenuClick} />
       </footer>
     );
   }
